a8d73d4e461f0acf25df948f514ef5c4ccd0e7b6
Damian <swean@vp.pl>
2025-08-03 15:02:54 +0200

    test
    Vanlife project

---
+++ Vanlife/src/api.js
@@ -0,0 +1,91 @@
+import { initializeApp } from "firebase/app"
+import {
+    getFirestore,
+    collection,
+    doc,
+    getDocs,
+    getDoc,
+    query,
+    where,
+    documentId
+} from "firebase/firestore/lite"
+
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD7USSTo9_cn7Hu-CRNsU_k7q3uLPGx0R8",
+  authDomain: "vanlife-d6f35.firebaseapp.com",
+  projectId: "vanlife-d6f35",
+  storageBucket: "vanlife-d6f35.appspot.com",
+  messagingSenderId: "382753424811",
+  appId: "1:382753424811:web:1c785601c57e156159beee"
+};
+
+
+
+const app = initializeApp(firebaseConfig)
+const db = getFirestore(app)
+
+
+const vansCollectionRef = collection(db, "vans")
+
+export async function getVans() {
+    const snapshot = await getDocs(vansCollectionRef)
+    const vans = snapshot.docs.map(doc => ({
+        ...doc.data(),
+        id: doc.id
+    }))
+    return vans
+}
+
+export async function getVan(id) {
+    const docRef = doc(db, "vans", id)
+    const snapshot = await getDoc(docRef)
+    return {
+        ...snapshot.data(),
+        id: snapshot.id
+    }
+}
+
+export async function getHostVans() {
+    const q = query(vansCollectionRef, where("hostId", "==", "123"))
+    const snapshot = await getDocs(q)
+    const vans = snapshot.docs.map(doc => ({
+        ...doc.data(),
+        id: doc.id
+    }))
+    return vans
+}
+
+
+
+// export async function getHostVan(id) {
+//     const q = query(
+//         vansCollectionRef,
+//         where(documentId(), "==", id),
+//         where("hostId", "==", "123")
+//     )
+//     const snapshot = await getDocs(q)
+//     const vans = snapshot.docs.map(doc => ({
+//         ...doc.data(),
+//         id: doc.id
+//     }))
+//     return vans[0]
+// }
+
+export async function loginUser(creds) {
+    const res = await fetch("/api/login",
+        { method: "post", body: JSON.stringify(creds) }
+    )
+    const data = await res.json()
+
+    if (!res.ok) {
+        throw {
+            message: data.message,
+            statusText: res.statusText,
+            status: res.status
+        }
+    }
+
+    return data
+}
